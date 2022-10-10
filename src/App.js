77d81f73fce1740ebff079a2717b8c4120551878
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-10-10 12:15:49 -0700

    added a footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
         <div className="">
             <Header />
             <Outlet />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="p-6 border-t border-primary-400/20 mt-20">
+            <h4 className="text-center">PowerBy Rasel Mahmud @{new Date().getFullYear()}</h4>
+        </div>
+    );
+};
+
+export default Footer;
